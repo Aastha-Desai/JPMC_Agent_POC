e20f863abcf5e81f4f3e8c8797dbf3a0853a6602
Aastha Desai <aastha.desai@mphasis.com>
2026-07-22 11:16:54 -0400

    create new press release added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import {Route, Routes} from "react-router";
 import Dashboard from "./pages/Dashboard";
+import NewPressRelease from "./pages/NewPressRelease";
+import Processing from "./pages/Processing";
 
 function App() {
-  return <Dashboard />;
+  return(
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/new" element={<NewPressRelease />} />
+      <Route path="/processing" element={<Processing />} />
+    </Routes>
+  );
 }
 
 export default App;
